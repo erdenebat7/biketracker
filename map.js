4000b9c3031b6da8425b878e8057671f1fdc34ab
erdenebat7 <erdenekhuub1@gmail.com>
2026-05-12 16:29:07 -0700

    map
    changed map color

--- map.js
+++ map.js
@@ -6,9 +6,33 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZXJkZW5lYmF0NyIsImEiOiJjbXAzNmV4aGkwbXdyMndwb
 
 const map = new mapboxgl.Map({
   container: 'map', 
-  style: 'mapbox://styles/erdenebat7/cmp37e8b1003001spgqmgf6pm', 
+  style: 'mapbox://styles/mapbox/outdoors-v12', 
   center: [-71.09415, 42.36027], 
   zoom: 12, 
   minZoom: 5, 
   maxZoom: 18, 
+});
+
+map.on('load', async () => {
+  map.addSource('boston_route', {
+  type: 'geojson',
+  data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+});
+  map.addLayer({
+  id: 'bike-lanes',
+  type: 'line',
+  source: 'boston_route',
+  paint: {
+    'line-color': 'green',
+    'line-width': 3,
+    'line-opacity': 0.4,
+  },
+});
+
+//
+// CAMBRIDGE BIKE LANES
+//
+
+
+
 });
